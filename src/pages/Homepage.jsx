93918c00a804bed2cdb6410fbe731407e8808fe4
TheHeisenBurglar <vidgames16@gmail.com>
2024-04-27 11:48:36 -0400

    added assets and tproll image

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,10 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 import { Navbar } from "../components";
+import tproll from "../assets/tp_roll.png"
 
 export default function Homepage(){
     return <Box>
         <Navbar />
-        <Image src=""/>
+        <Image src={tproll}/>
     </Box>
 }
