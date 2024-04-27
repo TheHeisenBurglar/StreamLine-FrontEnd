ab3ea9962751996a0d87da211985d4e44cdb5025
TheHeisenBurglar <vidgames16@gmail.com>
2024-04-27 11:24:33 -0400

    First commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+import { Navbar } from "../components";
+
+export default function Homepage(){
+    return <Box>
+        <Navbar />
+        <Image src=""/>
+    </Box>
+}
